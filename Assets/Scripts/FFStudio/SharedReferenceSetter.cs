e5e8af90779fd4f65937186e75fdcc6181a2c28f
ofgonen <ofgonen@gmail.com>
2021-06-01 22:00:39 +0300

    SharedReferenceSetter: Null the reference when on disable
    - Script Execution order: LevelManager and UIManager is before default time
    and SharedReferenceSetter is after default time

--- Assets/Scripts/FFStudio/SharedReferenceSetter.cs
+++ Assets/Scripts/FFStudio/SharedReferenceSetter.cs
@@ -1,21 +1,29 @@
+/* Created by and for usage of FF Studios (2021). */
+
 using System.Collections;
 using System.Collections.Generic;
+using FFStudio;
 using UnityEngine;
 
 namespace FFStudio
 {
-    public class SharedReferanceSetter : MonoBehaviour
-    {
-        #region Fields
-        public SharedReferenceProperty sharedReferanceProperty;
-        public Component referanceComponent;
-        #endregion
+	public class SharedReferenceSetter : MonoBehaviour
+	{
+		#region Fields
+		public SharedReferenceProperty sharedReferanceProperty;
+		public Component referanceComponent;
+		#endregion
 
-        #region UnityAPI
-        private void Awake()
-        {
-            sharedReferanceProperty.SetValue(referanceComponent);
-        }
-        #endregion
-    }
+		#region UnityAPI
+		private void OnEnable()
+		{
+			sharedReferanceProperty.SetValue( referanceComponent );
+		}
+
+		private void OnDisable()
+		{
+			sharedReferanceProperty.SetValue( null );
+		}
+		#endregion
+	}
 }
