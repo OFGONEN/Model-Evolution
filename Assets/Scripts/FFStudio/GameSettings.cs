4cac6a7fc0d6a3a12751df31786adbc2803917b0
Fauder <canik.burak@gmail.com>
2021-09-30 18:02:38 +0300

    GameSettings: Refactor according to convention

--- Assets/Scripts/FFStudio/GameSettings.cs
+++ Assets/Scripts/FFStudio/GameSettings.cs
@@ -7,6 +7,15 @@ namespace FFStudio
 {
 	public class GameSettings : ScriptableObject
     {
+#region Singleton Related
+        private static GameSettings instance;
+
+        private delegate GameSettings ReturnGameSettings();
+        private static ReturnGameSettings returnInstance = LoadInstance;
+
+		public static GameSettings Instance => returnInstance();
+#endregion
+        
 #region Fields
         public int maxLevelCount;
         [ Foldout( "UI Settings" ), Tooltip( "Duration of the movement for ui element"          ) ] public float ui_Entity_Move_TweenDuration;
@@ -14,23 +23,10 @@ namespace FFStudio
 		[ Foldout( "UI Settings" ), Tooltip( "Duration of the scaling for ui element"           ) ] public float ui_Entity_Scale_TweenDuration;
 		[ Foldout( "UI Settings" ), Tooltip( "Duration of the movement for floating ui element" ) ] public float ui_Entity_FloatingMove_TweenDuration;
         [ Foldout( "UI Settings" ), Tooltip( "Percentage of the screen to register a swipe"     ) ] public int swipeThreshold;
-
-        private static GameSettings instance;
-
-        private delegate GameSettings ReturnGameSettings();
-        private static ReturnGameSettings returnInstance = LoadInstance;
-
-        public static GameSettings Instance
-        {
-            get
-            {
-                return returnInstance();
-            }
-        }
 #endregion
 
 #region Implementation
-        static GameSettings LoadInstance()
+        private static GameSettings LoadInstance()
 		{
 			if( instance == null )
 				instance = Resources.Load< GameSettings >( "game_settings" );
@@ -40,7 +36,7 @@ namespace FFStudio
 			return instance;
 		}
 
-		static GameSettings ReturnInstance()
+		private static GameSettings ReturnInstance()
         {
             return instance;
         }
