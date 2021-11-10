6412d1813511928233b965c8025aace2d5ec2f38
Burak Canik <canik.burak@gmail.com>
2021-11-10 20:36:20 +0300

    Add Layer attribute for editor use

---
+++ Assets/Editor/LayerAttributeDrawer.cs
@@ -0,0 +1,19 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using UnityEditor;
+using UnityEngine;
+
+[ CustomPropertyDrawer( typeof( LayerAttribute ) ) ]
+public class LayerAttributeDrawer : PropertyDrawer
+{
+	public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+	{
+		if( property.propertyType != SerializedPropertyType.Integer )
+		{
+			EditorGUI.LabelField( position, "The property has to be a layer for LayerAttribute to work!" );
+			return;
+		}
+
+		property.intValue = EditorGUI.LayerField( position, label, property.intValue );
+	}
+}
---
+++ Assets/Script/FFStudio/LayerAttribute.cs
@@ -0,0 +1,7 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System;
+using UnityEngine;
+
+[ AttributeUsage( AttributeTargets.Field ) ]
+public class LayerAttribute : PropertyAttribute { }
