8bef5a0ce223f94615b735db188ab219af3866c3
ofgonen <ofgonen@gmail.com>
2021-12-23 17:50:40 +0300

    Bugfix @ExtensionMethod: UpdateSkinnedMeshRenderer using wrong bone array

--- Assets/Script/FFStudio/ExtensionMethods.cs
+++ Assets/Script/FFStudio/ExtensionMethods.cs
@@ -248,22 +248,24 @@ namespace FFStudio
 			}
 		}
 
-		public static void UpdateSkinnedMeshRenderer( this GameObject gameObject, SkinnedMeshRenderer currentRender, SkinnedMeshRenderer newRenderer )
-		{
-			currentRender.sharedMesh = newRenderer.sharedMesh;
+        public static void UpdateSkinnedMeshRenderer(this GameObject gameObject, SkinnedMeshRenderer currentRender, SkinnedMeshRenderer newRenderer)
+        {
+            currentRender.sharedMesh = newRenderer.sharedMesh;
+            currentRender.sharedMaterials = newRenderer.sharedMaterials;
+            currentRender.localBounds = newRenderer.localBounds;
 
-			baseModelBones.Clear();
-			targetModelBones.Clear();
+            baseModelBones.Clear();
+            targetModelBones.Clear();
 
-			gameObject.GetComponentsInChildren<Transform>( true, baseModelBones );
+            gameObject.GetComponentsInChildren<Transform>(true, baseModelBones);
 
-			for( int boneOrder = 0; boneOrder < newRenderer.bones.Length; boneOrder++ )
-			{
-				targetModelBones.Add( targetModelBones.Find( c => c.name == newRenderer.bones[ boneOrder ].name ) );
-			}
+            for (int boneOrder = 0; boneOrder < newRenderer.bones.Length; boneOrder++)
+            {
+                targetModelBones.Add(baseModelBones.Find(c => c.name == newRenderer.bones[boneOrder].name));
+            }
 
-			currentRender.bones = targetModelBones.ToArray();
-		}
+            currentRender.bones = targetModelBones.ToArray();
+        }
 
 		public static void SetFieldValue( this object source, string fieldName, string value )
 		{
