1bce02af5dfd122541239bf02625652682d7af86
Burak Canik <canik.burak@gmail.com>
2022-01-06 18:54:18 +0300

    UI Text Updater: Generic base class for trivial UI update operations

---
+++ Assets/Script/FFStudio/UI/UITextUpdater_Float.cs
@@ -0,0 +1,8 @@
+/* Created by and for usage of FF Studios (2021). */
+
+namespace FFStudio
+{
+    public class UITextUpdater_Float : UITextUpdater< SharedFloatNotifier, float >
+    {
+    }
+}
---
+++ Assets/Script/FFStudio/UI/UITextUpdater_Int.cs
@@ -0,0 +1,28 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITextUpdater_Int : MonoBehaviour
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
