bc55867b346b426b576ef00e0b1c8ae81dc70db0
Burak Canik <canik.burak@gmail.com>
2022-01-10 16:47:17 +0300

    Organize Scripts folder

--- Assets/Script/FFStudio/Data/SharedInput_JoyStick.cs
+++ Assets/Script/FFStudio/Data/SharedInput_JoyStick.cs
@@ -1,33 +1,27 @@
 /* Created by and for usage of FF Studios (2021). */
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
 
 [ CreateAssetMenu( fileName = "shared_input_joystick", menuName = "FF/Data/Shared/Input/JoyStick" ) ]
 public class SharedInput_JoyStick : SharedVector2Notifier
 {
-#region Fields
-
-    // Private \\
+#region Fields (Public)
+    public SafeEvent input_toggle; 
+#endregion
+    
+#region Fields (Private)
     private Vector2 input_received;
     private Vector2 input_screen_position;
     private Vector2 input_direction;
     private bool input_enabled;
-
-    // Events \\
-    public SafeEvent input_toggle; 
-#endregion
+#endregion	
 
 #region Properties
     public Vector2 Input_Received        => input_received;
     public Vector2 Input_Screen_Position => input_screen_position;
     public Vector2 Input_Direction       => input_direction;
     public bool Input_Enabled            => input_enabled;
-#endregion
-
-#region Unity API
 #endregion
 
 #region API
@@ -59,13 +53,5 @@ public class SharedInput_JoyStick : SharedVector2Notifier
     {
 		input_toggle = null;
 	}
-#endregion
-
-#region Implementation
-#endregion
-
-#region Editor Only
-#if UNITY_EDITOR
-#endif
 #endregion
 }
--- Assets/Script/FFStudio/Manager/AssetManager.cs
+++ Assets/Script/FFStudio/Manager/AssetManager.cs
@@ -5,7 +5,7 @@ using FFStudio;
 
 /* This class holds references to ScriptableObject assets. These ScriptableObjects are singletons, so they need to load before a Scene does.
  * Using this class ensures at least one script from a scene holds a reference to these important ScriptableObjects. */
-public class AppAssetHolder : MonoBehaviour
+public class AssetManager : MonoBehaviour
 {
 #region Fields
 	public GameSettings gameSettings;
