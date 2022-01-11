d754e018d2b795cd8a42f7ba5281662fdd58624d
Ömer Faruk Gönen <ofgonen@gmail.com>
2022-01-11 21:31:47 +0300

    Implement UI_Update_Image_FillAmount_Tiled

---
+++ Assets/Script/FFStudio/UI/UI_Update_Image_FillAmount_Tiled.cs
@@ -0,0 +1,55 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using FFStudio;
+
+public class UI_Update_Image_FillAmount_Tiled : MonoBehaviour
+{
+#region Fields
+    [ Range( 0.0f, 1.0f ) ]
+	public float stepSize;
+	public SharedFloatNotifier sharedDataNotifier;
+
+	private Image ui_Image;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+    private void OnEnable()
+    {
+		sharedDataNotifier.Subscribe( OnSharedDataChange );
+	}
+
+    private void OnDisable()
+    {
+		sharedDataNotifier.Unsubscribe( OnSharedDataChange );
+    }
+
+    private void Awake()
+    {
+        ui_Image = GetComponentInChildren< Image >();
+
+		OnSharedDataChange();
+	}
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+    private void OnSharedDataChange()
+    {
+		ui_Image.fillAmount = Mathf.FloorToInt( ( 1.0f - sharedDataNotifier.SharedValue ) / stepSize ) * stepSize;
+	}
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
