a7839ca065d5dcbd0037fc18ce8a94d7d78a94e7
ofgonen <ofgonen@gmail.com>
2022-03-12 19:55:56 +0300

    Bugfix @Dress: Cloth position offsets would accumulate

--- Assets/Script/Dress.cs
+++ Assets/Script/Dress.cs
@@ -57,7 +57,7 @@ public class Dress : MonoBehaviour
 
 		dress_mesh_renderer.sharedMaterials   = dress_data.dress_sharedMaterials;
 		dress_mesh_filter.mesh                = dress_data.dress_mesh;
-		dress_mesh_filter.transform.position += dress_data.dress_offset_position;
+		dress_mesh_filter.transform.position  = dress_data.dress_offset_position;
 	}
 #endregion
 
