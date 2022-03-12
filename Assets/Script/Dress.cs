c65e965d5e57468d3939d799fee92c7f8695f58b
ofgonen <ofgonen@gmail.com>
2022-03-12 14:09:51 +0300

    Edit cloth.prefab: Configure for Dress component
    - Create animator controller

---
+++ Assets/Script/Dress.cs
@@ -0,0 +1,33 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public class Dress : MonoBehaviour
+{
+#region Fields
+    [ BoxGroup( "Setup" ) ] public Animator animator;
+    [ BoxGroup( "Setup" ) ] public MeshRenderer dress_mesh_renderer;
+    [ BoxGroup( "Setup" ) ] public MeshFilter dress_mesh_filter;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
