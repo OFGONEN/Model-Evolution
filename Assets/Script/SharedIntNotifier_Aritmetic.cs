c8fb5a2b990a376a0a11efc2635505a76bad774a
Ömer Faruk Gönen <ofgonen@gmail.com>
2022-03-27 14:57:40 +0300

    SharedIntNotifier_Aritmetic: Add cooldown for editing the SharedValue
    - Prevent two door editing the value at the same frame

--- Assets/Script/SharedIntNotifier_Aritmetic.cs
+++ Assets/Script/SharedIntNotifier_Aritmetic.cs
@@ -5,31 +5,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
 using Sirenix.OdinInspector;
+using DG.Tweening;
 
 [ CreateAssetMenu( fileName = "notifier_", menuName = "FF/Data/Shared/Notifier/Float Aritmetic" ) ]
 public class SharedIntNotifier_Aritmetic : SharedIntNotifier
 {
+	public float cooldown;
+
+	private bool canInteract = true;
+
 	[ Button() ]
 	public void Add( int value )
 	{
-		SharedValue += value;
+		if( canInteract )
+		{
+			FFLogger.Log( "WTF", this );
+			SharedValue += value;
+			CoolDown();
+		}
 	}
 
 	[ Button() ]
 	public void Subtract( int value )
 	{
-		SharedValue -= value;
+		if( canInteract )
+		{
+			FFLogger.Log( "WTF", this );
+			SharedValue -= value;
+			CoolDown();
+		}
 	}
 
 	[ Button() ]
 	public void Multiply( int value )
 	{
-		SharedValue *= value;
+		if( canInteract )
+		{
+			FFLogger.Log( "WTF", this );
+			SharedValue *= value;
+			CoolDown();
+		}
 	}
 
 	[ Button() ]
 	public void Divide( int value )
 	{
-		SharedValue /= value;
+		if( canInteract )
+		{
+			FFLogger.Log( "WTF", this );
+			SharedValue /= value;
+			CoolDown();
+		}
+	}
+
+	private void CoolDown()
+	{
+		canInteract = false;
+		DOVirtual.DelayedCall( cooldown, OnCoolDownComplete );
+	}
+
+	private void OnCoolDownComplete()
+	{
+		canInteract = true;
 	}
 }
